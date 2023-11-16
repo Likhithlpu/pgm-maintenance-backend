11666cfafc9664fa0ba19a91d4ac79cc5314abbf
Likhith Kanigolla <likhithkanigolla@gmail.com>
2023-11-17 03:16:09 +0530

    Updated Chnages for db.js

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./db-old');
+const db = require('./db');
 
 
 // Define a route to store complaints in the database
